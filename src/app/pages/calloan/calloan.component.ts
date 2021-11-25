3d7d5c48165347e7dfd42be5f671c94ffad84953
kasun sankalpa <kasunsankalpa440@gmail.com>
2021-11-25 12:26:28 +0530

    alignment
    alignment

--- src/app/pages/calloan/calloan.component.ts
+++ src/app/pages/calloan/calloan.component.ts
@@ -39,7 +39,7 @@ export class CalloanComponent implements OnInit {
     if(this.loanamount && this.rate  && this.month){
       this.Capital=(this.loanamount /this.month).toFixed(2);
       this.Interest = (this.Capital*(this.rate/100)).toFixed(2);
-      this.Total=Number(this.Capital) + Number(this.Interest) ;
+      this.Total=(Number(this.Capital) + Number(this.Interest)) .toFixed(2);
       this.FullTotal= (Number(this.Total)*this.month).toFixed(2);
       this.ischeck=true;
 
