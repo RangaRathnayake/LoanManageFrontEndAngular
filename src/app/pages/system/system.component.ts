a55245799af091cd911bfd86e751081a939bab4e
kasun sankalpa kulasinghe <63305825+kasunsankalpa@users.noreply.github.com>
2021-12-14 16:22:16 +0530

    warrent
    warrent

--- src/app/pages/system/system.component.ts
+++ src/app/pages/system/system.component.ts
@@ -12,37 +12,84 @@ export class SystemComponent implements OnInit {
   rate;
   ratelist;
 
+  daycount;
+  rateperdate;
+
+  daycountlist;
+  rateperdatelist;
+
+
+  keyvallist;
+
   constructor(private apiCall: ApicallService, private alart: AlartService) { }
 
   ngOnInit(): void {
     this.getrate();
+    this.getwarrentdetails();
   }
 
-  apply(){
-    if(this.rate){
+  apply() {
+    if (this.rate) {
       this.apiCall.post('interest', {
-        interest:{
+        interest: {
           rate: this.rate,
           status: 1
-      }
-      }, data => { 
-        if(data){
+        }
+      }, data => {
+        if (data) {
           this.alart.showNotification('success', 'save');
           this.getrate();
         }
 
-       })
-    }else{
+      })
+    } else {
       this.alart.showNotification('warning', 'check all feilds');
     }
   }
 
-  getrate(){
+  getrate() {
     this.apiCall.get('interest', result => {
       this.ratelist = result;
       console.log(result);
 
     })
+  }
+
+  getwarrentdetails() {
+    this.apiCall.get('keyval/warrant_day', result => {
+      this.daycountlist = result;
+      this.daycount = result.val;
+      this.apiCall.get('keyval/warrant_rate', results => {
+        this.rateperdatelist = results;
+        this.rateperdate = results.val;
+      })
+    })
+  }
+
+
+  savewarrent() {
+    if (this.daycount && this.rateperdate) {
+      this.daycountlist.val = this.daycount;
+      this.rateperdatelist.val = this.rateperdate;
+      this.apiCall.post('keyval', {
+        keyval: this.daycountlist
+      }, data => {
+        if (data) {
+          this.apiCall.post('keyval', {
+            keyval: this.rateperdatelist
+          }, data => {
+            if (data) {
+              this.getwarrentdetails();
+              this.alart.showNotification('success', 'save');
+            }
+          })
+
+        }
+      })
+
+    } else {
+      this.alart.showNotification('warning', 'check all feilds');
+    }
   }
 
 }
