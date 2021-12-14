bb9df024c5d73346656d93d7f583ab2cd5348dbc
kasun sankalpa kulasinghe <63305825+kasunsankalpa@users.noreply.github.com>
2021-12-14 14:29:24 +0530

    sys
    sys

--- src/app/pages/system/system.component.ts
+++ src/app/pages/system/system.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ApicallService } from 'app/service/apicall.service';
+import { AlartService } from 'app/service/alart.service';
 
 @Component({
   selector: 'app-system',
@@ -8,15 +10,39 @@ import { Component, OnInit } from '@angular/core';
 export class SystemComponent implements OnInit {
 
   rate;
+  ratelist;
 
-  constructor() { }
+  constructor(private apiCall: ApicallService, private alart: AlartService) { }
 
   ngOnInit(): void {
+    this.getrate();
   }
 
   apply(){
+    if(this.rate){
+      this.apiCall.post('interest', {
+        interest:{
+          rate: this.rate,
+          status: 1
+      }
+      }, data => { 
+        if(data){
+          this.alart.showNotification('success', 'save');
+          this.getrate();
+        }
 
-    
+       })
+    }else{
+      this.alart.showNotification('warning', 'check all feilds');
+    }
+  }
+
+  getrate(){
+    this.apiCall.get('interest', result => {
+      this.ratelist = result;
+      console.log(result);
+
+    })
   }
 
 }
