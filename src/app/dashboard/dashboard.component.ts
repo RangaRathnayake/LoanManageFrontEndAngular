40d87ce7fb595dc0234ae5900330e29d4affc124
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2022-01-09 22:26:50 +0530

    update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ApicallService } from 'app/service/apicall.service';
 import * as Chartist from 'chartist';
 
 @Component({
@@ -8,10 +9,28 @@ import * as Chartist from 'chartist';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  menuItems;
+  constructor(private apiCall: ApicallService) { }
 
   ngOnInit() {
-     
+    this.getPrivilages();
+  }
+
+  getPrivilages() {
+
+    const user = this.apiCall.logedUser();
+    if (user) {
+      const ar = [];
+      this.apiCall.get('user/privilage/' + user.utype.id, (data) => {
+        data.privilages.forEach(element => {
+          const obj = { path: element.link, title: element.string, icon: element.icon, class: '' };
+          ar.push(obj);
+          this.menuItems = ar;
+          console.log(this.menuItems);
+        });
+      })
+    }
+
   }
 
 }
