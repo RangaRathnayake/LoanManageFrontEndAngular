49492f7ccb14cc60c8668aed75607a4956a330b8
Ranga Rathnayake <58025781+RangaRathnayake@users.noreply.github.com>
2022-04-08 10:34:06 +0530

    update

--- src/app/pages/report/report.component.ts
+++ src/app/pages/report/report.component.ts
@@ -1,9 +1,9 @@
-import { Component, OnInit } from "@angular/core";
-import { environment } from "environments/environment";
+import { Component, OnInit } from '@angular/core';
+import { environment } from 'environments/environment';
 @Component({
-  selector: "app-report",
-  templateUrl: "./report.component.html",
-  styleUrls: ["./report.component.scss"],
+  selector: 'app-report',
+  templateUrl: './report.component.html',
+  styleUrls: ['./report.component.scss'],
 })
 export class ReportComponent implements OnInit {
   from;
@@ -18,12 +18,12 @@ export class ReportComponent implements OnInit {
       range: {
         from: this.from,
         to: this.to,
-        type: "L",
+        type: 'L',
       },
     };
     window.open(
-      this.reportPath + "all.html?data=" + JSON.stringify(range),
-      "_blank"
+      this.reportPath + 'all.html?data=' + JSON.stringify(range),
+      '_blank'
     );
   }
 
@@ -32,12 +32,12 @@ export class ReportComponent implements OnInit {
       range: {
         from: this.from,
         to: this.to,
-        type: "L",
+        type: 'L',
       },
     };
     window.open(
-      this.reportPath + "incomeLoan.html?data=" + JSON.stringify(range),
-      "_blank"
+      this.reportPath + 'incomeLoan.html?data=' + JSON.stringify(range),
+      '_blank'
     );
   }
 
@@ -46,12 +46,12 @@ export class ReportComponent implements OnInit {
       range: {
         from: this.from,
         to: this.to,
-        type: "P",
+        type: 'P',
       },
     };
     window.open(
-      this.reportPath + "incomeProp.html?data=" + JSON.stringify(range),
-      "_blank"
+      this.reportPath + 'incomeProp.html?data=' + JSON.stringify(range),
+      '_blank'
     );
   }
 
@@ -63,8 +63,8 @@ export class ReportComponent implements OnInit {
       },
     };
     window.open(
-      this.reportPath + "expences.html?data=" + JSON.stringify(range),
-      "_blank"
+      this.reportPath + 'expences.html?data=' + JSON.stringify(range),
+      '_blank'
     );
   }
 
@@ -76,9 +76,19 @@ export class ReportComponent implements OnInit {
       },
     };
     window.open(
-      this.reportPath + "arrearsReport.html?data=" + JSON.stringify(range),
-      "_blank"
+      this.reportPath + 'arrearsReport.html?data=' + JSON.stringify(range),
+      '_blank'
     );
+  }
+
+  creditMain() {
+    var range = {
+      range: {
+        from: this.from,
+        to: this.to,
+      },
+    };
+    window.open(this.reportPath + 'main.html', '_blank');
   }
 
   newloandetails() {
@@ -89,12 +99,21 @@ export class ReportComponent implements OnInit {
       },
     };
     window.open(
-      this.reportPath + "newloandetails.html?data=" + JSON.stringify(range),
-      "_blank"
+      this.reportPath + 'newloandetails.html?data=' + JSON.stringify(range),
+      '_blank'
     );
   }
 
-  incomeexpence() {
-    window.open(this.reportPath + "incomeexpence.html?data=", "_blank");
+  incomeexpenceLoan() {
+    console.log('hit');
+    var type = { type: 'L' };
+    window.open(
+      this.reportPath + 'incomeexpence.html?data=' + JSON.stringify(type),
+      '_blank'
+    );
+  }
+
+  incomeexpenceProperty() {
+    window.open(this.reportPath + 'incomeexpence.html?data=P', '_blank');
   }
 }
